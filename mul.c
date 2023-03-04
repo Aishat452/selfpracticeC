5edac9e812e0451e56b03cd1c7dcf84ca1953120
Aishat452 <aishadeperfectly@gmail.com>
2023-03-04 15:58:31 +0100

    compiled the hello world file

---
+++ mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main (result)
+	int a = 2;
+	int b = 3;
+	int a * b = result;
+
+	if ("%d %d", &a * &b)
+{
+
+	a * b = mul;
+		printf("Correct answer");
+                return (result);
+} else{
+   printf("not correct")
+}
