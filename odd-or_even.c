b733e5ac1ff778d71932948f2166e1b4dee52c1c
Aishat452 <aishadeperfectly@gmail.com>
2023-03-07 12:34:23 +0100

    odd or even

---
+++ odd-or_even.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Create a program to check whether a number is odd or even
+* Use a tenary operator to check if the number is odd or even
+* if number is odd: print, the number is odd
+* if number is even, print the number is even
+* Return: 0
+*/
+
+int main()
+{
+
+int num;
+int result = num % 2;
+
+printf("Kindly input your number below\n");
+printf("=>");
+scanf("%d", num);
+
+(result == result) ? printf("Your number %d is even."): printf("Your number %d is odd.");
+
+return (0);
+
+}
